9edb21c14950f5725244f11b6c5ca6c1e357039c
Adriano Saadeh <adrianosaadeh@gmail.com>
2025-03-09 09:39:20 -0300

    resolvendo sozinho

--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -18,8 +18,20 @@ function sortear() {
         
         sorteados.push(num);
     }
-
     document.getElementById('resultado').innerHTML =`<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`;
+    var botao = document.getElementById('btn-reiniciar');
+    botao.classList.replace('container__botao-desabilitado', 'container__botao');
+}
+
+function reiniciar() {
+    document.getElementById('quantidade').value = "";
+    document.getElementById('de').value = "";
+    document.getElementById('ate').value = "";
+
+    var botao = document.getElementById('btn-reiniciar');
+    botao.classList.replace('container__botao', 'container__botao-desabilitado');
+
+    document.querySelector('#resultado .texto__paragrafo').textContent = 'Números sorteados: nenhum até agora';
 }
 
 function obterNumeroAleatorio(min, max) {
