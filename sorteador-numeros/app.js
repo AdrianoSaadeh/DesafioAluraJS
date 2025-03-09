2a151b15113930b1e71dec3b3a1e0eec9e7a276a
Adriano Saadeh <adrianosaadeh@gmail.com>
2025-03-09 09:55:09 -0300

    refatorando com metodos

--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -1,26 +1,33 @@
-
-
 function sortear() {
     var qtd = parseInt(document.getElementById('quantidade').value);
     var de = parseInt(document.getElementById('de').value);
     var ate = parseInt(document.getElementById('ate').value);
-    
+
     var sorteados = [];
     var num;
 
     for (let index = 0; index < qtd; index++) {
-        
+
         num = obterNumeroAleatorio(de, ate);
 
         while (sorteados.includes(num)) {
             num = obterNumeroAleatorio(de, ate);
         }
-        
+
         sorteados.push(num);
     }
-    document.getElementById('resultado').innerHTML =`<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`;
+    document.getElementById('resultado').innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`;
+
+    alterarStattusBotao();
+}
+
+function alterarStattusBotao() {
     var botao = document.getElementById('btn-reiniciar');
-    botao.classList.replace('container__botao-desabilitado', 'container__botao');
+    if (botao.classList.contains('container__botao-desabilitado')) {
+        botao.classList.replace('container__botao-desabilitado', 'container__botao');
+    } else {
+        botao.classList.replace('container__botao-desabilitado', 'container__botao');
+    }
 }
 
 function reiniciar() {
@@ -28,12 +35,11 @@ function reiniciar() {
     document.getElementById('de').value = "";
     document.getElementById('ate').value = "";
 
-    var botao = document.getElementById('btn-reiniciar');
-    botao.classList.replace('container__botao', 'container__botao-desabilitado');
-
     document.querySelector('#resultado .texto__paragrafo').textContent = 'Números sorteados: nenhum até agora';
+
+    alterarStattusBotao();
 }
 
 function obterNumeroAleatorio(min, max) {
-    return Math.floor(Math.random() * (max - min) + min); 
+    return Math.floor(Math.random() * (max - min) + min);
 }
