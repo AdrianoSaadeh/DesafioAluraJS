bdc1c220fcd93f25d514c6056f07fb104f85ab74
Adriano Saadeh <adrianosaadeh@gmail.com>
2025-03-09 11:58:08 -0300

    solução do alura

--- alugames/js/app.js
+++ alugames/js/app.js
@@ -1,16 +1,34 @@
-function alterarStatus(num) {
+function alterarStatus(id) {
 
-    if (num === 1) {
-        botao = document.querySelector('#game-1 .dashboard__item__button');
-        botao.classList.add('dashboard__item__button--return');
-        botao.textContent = 'Devolver'
-    } else if (num === 2) {
-        botao = document.querySelector('#game-2 .dashboard__item__button');
-        botao.classList.add('dashboard__item__button--return');
-        botao.textContent = 'Devolver'
-    } else {
-        botao = document.querySelector('#game-3 .dashboard__item__button');
+    // minha implementação
+
+    // if (id === 1) {
+    //     botao = document.querySelector('#game-1 .dashboard__item__button');
+    //     botao.classList.add('dashboard__item__button--return');
+    //     botao.textContent = 'Devolver'
+    // } else if (id === 2) {
+    //     botao = document.querySelector('#game-2 .dashboard__item__button');
+    //     botao.classList.add('dashboard__item__button--return');
+    //     botao.textContent = 'Devolver'
+    // } else {
+    //     botao = document.querySelector('#game-3 .dashboard__item__button');
+    //     botao.classList.remove('dashboard__item__button--return');
+    //     botao.textContent = 'Alugar'
+    // }
+
+    // Solução da Alura
+
+    let gameClicado = document.getElementById(`game-${id}`);
+    let imagem = gameClicado.querySelector('.dashboard__item__img');
+    let botao = gameClicado.querySelector('.dashboard__item__button');
+
+    if (imagem.classList.contains('dashboard__item__img--rented')) {
+        imagem.classList.remove('dashboard__item__img--rented');
         botao.classList.remove('dashboard__item__button--return');
-        botao.textContent = 'Alugar'
+        botao.textContent = 'Alugar';
+    } else {
+        imagem.classList.add('dashboard__item__img--rented');
+        botao.classList.add('dashboard__item__button--return');
+        botao.textContent = 'Devolver';
     }
 }
