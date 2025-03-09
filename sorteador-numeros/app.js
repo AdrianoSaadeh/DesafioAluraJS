562d5f653019330017e9768f784285cc6cf643a4
Adriano Saadeh <adrianosaadeh@gmail.com>
2025-03-09 10:28:08 -0300

    validações extras

--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -3,6 +3,16 @@ function sortear() {
     var de = parseInt(document.getElementById('de').value);
     var ate = parseInt(document.getElementById('ate').value);
 
+    if (de >= ate) {
+        alert('Campo "Do número" deve ser inferior ao campo "Até o número". Verifique!');
+        return;
+    }
+
+    if (qtd > (ate - de + 1)) {
+        alert('Campo "Quantidade" deve ser menor ou igual ao intervalo informado no campo "Do número" até o campo "Até o número". Verifique!');
+        return;
+    }
+
     var sorteados = [];
     var num;
 
@@ -12,6 +22,7 @@ function sortear() {
 
         while (sorteados.includes(num)) {
             num = obterNumeroAleatorio(de, ate);
+
         }
 
         sorteados.push(num);
@@ -41,5 +52,5 @@ function reiniciar() {
 }
 
 function obterNumeroAleatorio(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
