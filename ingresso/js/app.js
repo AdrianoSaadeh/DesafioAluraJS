b1dcf3081fe245b28bacd5d11982ec1d0adcf971
Adriano Saadeh <adrianosaadeh@gmail.com>
2025-03-10 08:59:42 -0300

    melhorando

--- ingresso/js/app.js
+++ ingresso/js/app.js
@@ -1,12 +1,13 @@
 function comprar() {
-    // Recuperar tipo de ingresso
+    // Recuperar ingresso e qtd selecionados
     let ingresso = document.getElementById('tipo-ingresso').value;
-    let qtd = document.getElementById('qtd').value;
+    let qtd = parseInt(document.getElementById('qtd').value);
 
-    // Validar a quantidade de tipo-ingresso disponíveis
+    // Recuperar quantidades disponíveis
     let elementoQtd = document.getElementById(`qtd-${ingresso}`);
     let qtdDisponivel = parseInt(elementoQtd.textContent);
 
+    // Validar a quantidade de tipo-ingresso disponíveis
     if (qtd > qtdDisponivel) {
         alert(`Quantidade de ingresso(s) ${ingresso} excede a disponível para venda`)
         return;
