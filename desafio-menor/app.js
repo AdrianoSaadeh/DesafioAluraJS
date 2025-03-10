e5e2ee4725c2ccfedd58993fed35ed3f69a15fab
Adriano Saadeh <adrianosaadeh@gmail.com>
2025-03-10 09:30:40 -0300

    desafios

--- desafio-menor/app.js
+++ desafio-menor/app.js
@@ -9,4 +9,46 @@ function capturarValores() {
     const numerosSeparados = "10,20,30,40,50";
     const arrayNumeros = numerosSeparados.split(',');
     console.log(arrayNumeros);
+
+    // Calculadora
+
+    function adicao(a, b) {
+        return a + b;
+    }
+
+    function subtracao(a, b) {
+        return a - b;
+    }
+
+    function multiplicacao(a, b) {
+        return a * b;
+    }
+
+    function divisao(a, b) {
+        if (b !== 0) {
+            return a / b;
+        } else {
+            return "Erro: divisão por zero.";
+        }
+    }
+
+    function calculadora(operacao, a, b) {
+        switch (operacao) {
+            case 'soma':
+                return adicao(a, b);
+            case 'subtracao':
+                return subtracao(a, b);
+            case 'multiplicacao':
+                return multiplicacao(a, b);
+            case 'divisao':
+                return divisao(a, b);
+            default:
+                return "Operação inválida.";
+        }
+    }
+
+    // Exemplo de uso
+    let resultado = calculadora('divisao', 5, 3);
+    console.log(resultado); // Saída esperada: 8
+
 }
